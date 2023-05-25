e8dff1f4fd6cfa5aba4a22a2dcede17887f24ddd
Gilad-S <gilad@savoray.com>
2023-05-25 15:25:00 +0300

    Changes by ranof

--- src/main/java/ap/ex2/BookScrabbleServer/BookScrabbleClient.java
+++ src/main/java/ap/ex2/BookScrabbleServer/BookScrabbleClient.java
@@ -1,6 +1,8 @@
 package ap.ex2.BookScrabbleServer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -8,13 +10,35 @@ import java.util.Scanner;
 public class BookScrabbleClient {
     private String ip;
     private int port;
+    private BufferedReader inFromClient;
+    private PrintWriter outToClient;
+    private Socket mySocket;
 
     public BookScrabbleClient(String ip, int port) {
         this.ip = ip;
         this.port = port;
+
     }
 
-    public boolean runClient(String query) throws IOException {
+    public void establishConnection() throws Exception{
+        this.mySocket = new Socket(this.ip, this.port);
+        System.out.println("GUEST SOCKET CREATED");
+        this.inFromClient = new BufferedReader(new InputStreamReader(this.mySocket.getInputStream()));
+        this.outToClient = new PrintWriter(this.mySocket.getOutputStream());
+
+//        Socket mySocket = new Socket(this.hostIP, this.hostPort);
+//        this.myHandler = new MyClientHandler(mySocket);
+//        this.myHandler.addObserver(this);
+//        this.myHandler.startHandlingClient();
+//        this.myHandler.sendMsg(Protocol.GUEST_LOGIN_REQUEST + this.getGameInstance().getNickname());
+
+    }
+
+    public void sendMsg(String msgToSend) {
+        this.outToClient.println(msgToSend);
+    }
+
+    /*public boolean runClient(String query) throws IOException {
         Socket bookServer = new Socket(this.ip, this.port);
         PrintWriter out = new PrintWriter(bookServer.getOutputStream(), true);
         Scanner in = new Scanner(bookServer.getInputStream());
@@ -22,11 +46,30 @@ public class BookScrabbleClient {
 
         String res = in.nextLine();
 
+
         in.close();
         out.close();
         bookServer.close();
 
         return res.equals("true");
+    }*/
+
+    public static void runClient(int port,String query,boolean result) {
+        try {
+            Socket server=new Socket("localhost",port);
+            PrintWriter out=new PrintWriter(server.getOutputStream());
+            Scanner in=new Scanner(server.getInputStream());
+            out.println(query);
+            out.flush();
+            String res=in.nextLine();
+            if((result && !res.equals("true")) || (!result && !res.equals("false")))
+                System.out.println("problem getting the right answer from the server (-10)");
+            in.close();
+            out.close();
+            server.close();
+        } catch (IOException e) {
+            System.out.println("your code ran into an IOException (-10)");
+        }
     }
 
     public static void main(String[] args) throws IOException {
